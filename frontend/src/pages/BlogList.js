cca98f79d61905df3b44d84db8c772261cb3601a
yuka toshima <tb19321yj@outlook.com>
2022-11-07 02:02:25 +1100

    blog and sites top image

--- frontend/src/pages/BlogList.js
+++ frontend/src/pages/BlogList.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import blogTopImage from "../material/blogSection.jpg";
 
 const BlogList = () => {
   return (
-    <>
-      <Container>BlogList</Container>
-    </>
+    <Container>
+      <figure className="position-relative">
+        <img src={blogTopImage} alt="blog" className="img-fluid pageImg" />
+        <figcaption>
+          <h1>BLOG</h1>
+        </figcaption>
+      </figure>
+    </Container>
   );
 };
 
--- frontend/src/pages/SitesList.js
+++ frontend/src/pages/SitesList.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import siteTopImage from "../material/sitesSection.jpg";
 
 const SitesList = () => {
-  return <div>SitesList</div>;
+  return (
+    <Container>
+      <figure className="position-relative">
+        <img src={siteTopImage} alt="sites" className="img-fluid pageImg" />
+        <figcaption>
+          <h1>FIND YOUR WAY</h1>
+        </figcaption>
+      </figure>
+    </Container>
+  );
 };
 
 export default SitesList;
