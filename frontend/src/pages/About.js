b2aeb2ed37d162a89159548ab6f3f50c7790bc60
yuka toshima <tb19321yj@outlook.com>
2022-11-06 23:04:57 +1100

    about page (used dammy image)

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,5 +1,10 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
+import Badge from "react-bootstrap/Badge";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const About = () => {
   const testImages = [
@@ -14,16 +19,40 @@ const About = () => {
   ];
 
   return (
-    <Carousel>
-      {testImages.map(({ src, alt }, id) => (
-        <Carousel.Item key={id}>
-          <img className="d-block w-100" src={src} alt={alt} />
-          <Carousel.Caption>
-            <h2>Explore Australia</h2>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <>
+      <Carousel>
+        {testImages.map(({ src, alt }, id) => (
+          <Carousel.Item key={id}>
+            <img className="d-block w-100" src={src} alt={alt} />
+            <Carousel.Caption>
+              <h2>Explore Australia</h2>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+
+      <h2 className="text-center m-5">
+        <Badge bg="secondary">ABOUT US</Badge>
+      </h2>
+      <Container>
+        <Row className="d-flex align-items-center">
+          <Col md="auto">
+            <img
+              className="imgTop mx-auto d-block"
+              src="https://images.unsplash.com/photo-1591701729564-3b5325d5a4bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
+              alt="top"
+            />
+          </Col>
+          <Col className="text-center">
+            <p>
+              We help travelers find the best destinations in Australia. Let's
+              make it your best Australia trip.
+            </p>
+            <Button variant="info">Let's start!</Button>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 };
 
