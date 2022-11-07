db40197c85c76a67111a24761f9b5d1ad8f669b4
Todd0554 <toddy9229@outlook.com>
2022-11-07 18:50:58 +1100

    users and sites API test finish

--- backend/test/app.test.js
+++ backend/test/app.test.js
@@ -1,14 +1,16 @@
 import request from 'supertest'
 import app from '../app.js'
 import mongoose from 'mongoose'
-import {Site} from '../models/siteModel.js'
-import {User} from '../models/userModel.js'
+import Site from '../models/siteModel.js'
+import User from '../models/userModel.js'
 
 beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  
 })
 
 afterAll(async () => {
+
   await mongoose.connection.close()
 })
 
@@ -22,12 +24,24 @@ describe("GET /", () => {
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// test sites server '/api/sites'
+// test sites API
 describe("sites", () => {
 
     describe('GET /api/sites', () => {
       it("should respond with 200", async () => {
         await request(app).get(`/api/sites`).expect(200)
+      })
+    })
+
+    describe('POST /api/sites', () => {
+      it("should post a new site", async () => {
+        const response = await request(app).post('/api/users/login').send({
+          email: "admin@example.com",
+          password: "123456"
+        });
+        const token = await response.body.token;
+        const site = await request(app).post(`/api/sites`).set('Content-Type', 'application/json').set('Authorization', `Bearer ${token}`)
+        expect(site.body.name).toBe("name of the site");
       })
     })
 
@@ -46,26 +60,45 @@ describe("sites", () => {
       })
     })
 
-    // describe('only for admin', () => {
+    describe('PUT /api/sites/:id', () => {
+      it("should post a new site", async () => {
+        const response = await request(app).post('/api/users/login').send({
+          email: "admin@example.com",
+          password: "123456"
+        });
+        const token = await response.body.token;
+        const targetSite = await Site.findOne({name: "name of the site"})
+        const targetSiteId = targetSite._id
+        const site = await request(app).put(`/api/sites/${targetSiteId}`).send({
+          name: "change name",
+          description: "The Big Banana is Australia's ",
+          rating: 4.0,
+          numComments: 5,
+          image: "/images/bigbanana.jpg",
+          category: "park"
+        }).set('Content-Type', 'application/json').set('Authorization', `Bearer ${token}`)
+        expect(site.body.name).toBe("change name");
+      })
+    })
 
-    //   describe('POST /api/sites', () => {
-    //     it("should post a new site", async () => {
-    //       const response = await request(app).post('/users/login').send({
-    //         email: "admin@example.com",
-    //         password: "123456"
-    //       });
-    //       const token = response.body.token;
-    //       console.log(token)
-    //       const site = await request(app).post(`/api/sites`).set('Content-Type', "application/json").set('Authorization', `Bearer ${token}`)
-    //       expect(site.body.name).toBe('name of the site');
-    //     })
-    //   })
-    // })
-    
+    describe("DELETE api/sites/:id", () => {
+      it("should delete a site", async () => {
+        const response = await request(app).post('/api/users/login').send({
+          email: "admin@example.com",
+          password: "123456"
+        });
+        const token = await response.body.token;
+        const targetSite = await Site.findOne({name: "change name"})
+        const targetSiteId = targetSite._id
+        const site = await request(app).delete(`/api/sites/${targetSiteId}`).set('Content-Type', 'application/json').set('Authorization', `Bearer ${token}`)
+        expect(site.statusCode).toBe(200);
+      })
+    })
 })
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+// test users API
 describe("users", () => {
   describe("POST api/users/login", () => {
     it("should response with 200", async () => {
@@ -78,19 +111,18 @@ describe("users", () => {
     })
   })
 
-  // describe("POST api/users/register", () => {
-  //   it("should response with 200", async () => {
-  //     const response = await request(app).post('/api/users/register').send({
-  //       name: "heyhey",
-  //       email: 'heyhey@example.com',
-  //       password: '123456'
-  //     })
-  //     expect(response.statusCode).toBe(201)
-  //     expect(response.body.name).toBe('heyhey')
-  //     const user = await User.findById(response.body._id)
-  //     await user.remove()
-  //   })
-  // })
+  describe("POST api/users/register", () => {
+    it("should response with 200", async () => {
+      const response = await request(app).post('/api/users/register').send({
+        name: "heyhey",
+        email: 'heyhey@example.com',
+        password: '123456'
+      })
+      expect(response.statusCode).toBe(201)
+      expect(response.body.name).toBe('heyhey')
+      // this new user will be deleted in the deleting test "DELETE api/users/:id"
+    })
+  })
 
   describe("GET api/users", () => {
     it("should response with 200", async () => {
@@ -101,10 +133,48 @@ describe("users", () => {
       const token = await admin.body.token
       const response = await request(app).get('/api/users').set('Authorization', `Bearer ${token}`)
       expect(response.statusCode).toBe(200)
-      
     })
   })
 
+  describe("GET api/users/profile", () => {
+    it("should response with 200", async () => {
+      const admin = await request(app).post('/api/users/login').send({
+        email: 'todd@example.com',
+        password: '123456'
+      })
+      const token = await admin.body.token
+      const response = await request(app).get('/api/users/profile').set('Authorization', `Bearer ${token}`)
+      expect(response.body.name).toBe('todd')
+    })
+  })
+
+  describe('PUT /api/users/profile', () => {
+    it("should post a new site", async () => {
+      const response = await request(app).post('/api/users/login').send({
+        email: "heyhey@example.com",
+        password: "123456"
+      });
+      const token = await response.body.token;
+      const res = await request(app).put(`/api/users/profile`).send({
+        name: "hey",
+      }).set('Content-Type', 'application/json').set('Content-Type', 'application/json').set('Authorization', `Bearer ${token}`)
+      expect(res.body.name).toBe("hey");
+    })
+  })
+
+  describe("DELETE api/users/:id", () => {
+    it("should response with 200", async () => {
+      const admin = await request(app).post('/api/users/login').send({
+        email: 'admin@example.com',
+        password: '123456'
+      })
+      const token = await admin.body.token
+      const targetUser = await User.findOne({name: 'hey'})
+      const targetUserId = targetUser._id
+      const response = await request(app).delete(`/api/users/${targetUserId}`).set('Authorization', `Bearer ${token}`)
+      expect(response.statusCode).toBe(200)
+    })
+  })
 })
 
 
