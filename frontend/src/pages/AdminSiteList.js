0bc08d7acc81c0c000836dd6544b6da726013587
yuka toshima <tb19321yj@outlook.com>
2022-11-08 18:45:35 +1100

    admin user list (styling)

--- frontend/src/pages/AdminSiteList.js
+++ frontend/src/pages/AdminSiteList.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 import { Container, Row, Col, Button } from "react-bootstrap";
-
 import Table from "react-bootstrap/Table";
 
 const AdminSiteList = () => {
@@ -14,63 +13,59 @@ const AdminSiteList = () => {
   ];
 
   return (
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col xs={12} md={6}>
-          <Link to="/">
-            <FontAwesomeIcon
-              icon={solid("arrow-left")}
-              style={{ width: 28, height: 23, color: "black" }}
-              className="mt-5"
-              variant="primary"
-            />
-          </Link>
-          <div className="text-center">
-            <h2 className="mb-4">SITE LIST</h2>
-            <Link to="/sites/:id/edit">
-              <Button variant="info" className="px-4">
-                CREATE A SITE
-              </Button>
-            </Link>
-          </div>
-        </Col>
+    <Container className="justify-content-md-center">
+      <Link to="/">
+        <FontAwesomeIcon
+          icon={solid("arrow-left")}
+          style={{ width: 28, height: 23, color: "black" }}
+          className="mt-5"
+          variant="primary"
+        />
+      </Link>
+      <div className="text-center">
+        <h2 className="mb-4">SITE LIST</h2>
+        <Link to="/sites/:id/edit">
+          <Button variant="info" className="px-4">
+            CREATE A SITE
+          </Button>
+        </Link>
+      </div>
 
-        <Table
-          striped
-          bordered
-          hover
-          responsive
-          className="table-sm text-center my-5 text-nowrap"
-        >
-          <thead>
-            <tr>
-              <th className="p-2">ID</th>
-              <th className="p-2">TITLE</th>
-              <th className="p-2">CATEGORY</th>
-              <th className="p-2"></th>
-            </tr>
-          </thead>
-          <tbody>
-            {dammySites.map(({ id, title, category }) => (
-              <tr key={id}>
-                <td className="p-2">{id}</td>
-                <td className="p-2">{title}</td>
-                <td className="p-2">{category}</td>
-                <td className="p-2">
-                  <Link to="/sites/:id/edit">
-                    <Button variant="info" className="btn-sm">
-                      <FontAwesomeIcon icon={solid("pen-to-square")} />
-                    </Button>
-                  </Link>
-                  <Button variant="danger" className="btn-sm">
-                    <FontAwesomeIcon icon={solid("trash-can")} />
+      <Table
+        striped
+        bordered
+        hover
+        responsive
+        className="table-sm text-center my-5 text-nowrap"
+      >
+        <thead>
+          <tr>
+            <th className="p-2">ID</th>
+            <th className="p-2">TITLE</th>
+            <th className="p-2">CATEGORY</th>
+            <th className="p-2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {dammySites.map(({ id, title, category }) => (
+            <tr key={id}>
+              <td className="p-2">{id}</td>
+              <td className="p-2">{title}</td>
+              <td className="p-2">{category}</td>
+              <td className="p-2">
+                <Link to="/sites/:id/edit">
+                  <Button variant="info" className="btn-sm">
+                    <FontAwesomeIcon icon={solid("pen-to-square")} />
                   </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Row>
+                </Link>
+                <Button variant="danger" className="btn-sm">
+                  <FontAwesomeIcon icon={solid("trash-can")} />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Container>
   );
 };
--- frontend/src/pages/AdminUserList.js
+++ frontend/src/pages/AdminUserList.js
@@ -1,7 +1,92 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
+import { Container, Row, Col, Button } from "react-bootstrap";
+import Table from "react-bootstrap/Table";
 
 const AdminUserList = () => {
-  return <div>AdminUserList</div>;
+  const dammyUsers = [
+    {
+      id: "10000000000",
+      username: "ABC",
+      email: "user1@example.com",
+      admin: false,
+    },
+    {
+      id: "20000000000",
+      username: "DEF",
+      email: "user2@example.com",
+      admin: true,
+    },
+    {
+      id: "30000000000",
+      username: "GHI",
+      email: "user3@example.com",
+      admin: false,
+    },
+  ];
+
+  return (
+    <Container className="justify-content-md-center">
+      <Link to="/">
+        <FontAwesomeIcon
+          icon={solid("arrow-left")}
+          style={{ width: 28, height: 23, color: "black" }}
+          className="mt-5"
+          variant="primary"
+        />
+      </Link>
+      <h2 className="mb-4 text-center">USER LIST</h2>
+
+      <Table
+        striped
+        bordered
+        hover
+        responsive
+        className="table-sm text-center my-5 text-nowrap"
+      >
+        <thead>
+          <tr>
+            <th className="p-2">ID</th>
+            <th className="p-2">USERNAME</th>
+            <th className="p-2">EMAIL</th>
+            <th className="p-2">ADMIN</th>
+            <th className="p-2"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {dammyUsers.map(({ id, username, email, admin }) => (
+            <tr key={id}>
+              <td className="p-2">{id}</td>
+              <td className="p-2">{username}</td>
+              <td className="p-2">{email}</td>
+              {admin ? (
+                <td className="p-2">
+                  <FontAwesomeIcon
+                    icon={solid("check")}
+                    style={{ color: "green" }}
+                  />
+                </td>
+              ) : (
+                <td className="p-2">
+                  <FontAwesomeIcon
+                    icon={solid("xmark")}
+                    style={{ color: "red" }}
+                  />
+                </td>
+              )}
+              <td className="p-2">
+                <Button variant="danger" className="btn-sm">
+                  <FontAwesomeIcon icon={solid("trash-can")} />
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
+  );
 };
 
 export default AdminUserList;
