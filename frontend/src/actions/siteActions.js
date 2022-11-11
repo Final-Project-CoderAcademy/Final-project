b55efb36191df2e25f9f64cd56ad5c84d4c5fb01
yuka toshima <tb19321yj@outlook.com>
2022-11-11 21:10:36 +1100

    get site detail data from backend

--- frontend/src/actions/siteActions.js
+++ frontend/src/actions/siteActions.js
@@ -16,7 +16,7 @@ import {
   SITE_DELETE_REQUEST,
   SITE_DELETE_SUCCESS,
   SITE_DELETE_FAIL,
-} from "../contents/listContents";
+} from "../contents/siteContents";
 import axios from "axios";
 
 // Get all the sites
@@ -28,6 +28,23 @@ export const allSites = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: SITES_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// Get one site detail
+export const siteDetail = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: SITE_DETAIL_REQUEST });
+    const { data } = await axios.get(`/api/sites/${id}`);
+    dispatch({ type: SITE_DETAIL_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: SITE_DETAIL_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/pages/SiteDetail.js
+++ frontend/src/pages/SiteDetail.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { siteDetail } from "../actions/siteActions";
 
 const SiteDetail = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const siteDetails = useSelector((state) => state.siteDetails);
+  const { error, site } = siteDetails;
+
+  useEffect(() => {
+    dispatch(siteDetail(id));
+  }, [dispatch, id]);
+
   const dammyComment = [
     {
       username: "ABC",
@@ -29,25 +42,12 @@ const SiteDetail = () => {
   return (
     <Container className="px-sm-5 mt-5">
       <img
-        src="https://images.unsplash.com/photo-1636220506380-30a272f09562?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
-        alt="site"
+        src={site.image}
+        alt={site.name}
         className="my-sm-5 img-fluid mx-auto d-block"
       />
-      <h3 className="mt-5 fw-bold">Title</h3>
-      <p className="lh-lg fs-6">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed
-        imperdiet libero. Vestibulum tincidunt ultricies tincidunt. Nullam a
-        neque quis orci mattis sollicitudin id quis quam. Nunc sed placerat
-        libero, quis sodales justo. Quisque sit amet ligula egestas metus
-        faucibus mattis. Mauris et imperdiet dolor, et elementum magna. Maecenas
-        lorem felis, luctus quis est in, ultricies tristique velit. Mauris nec
-        leo vel ipsum dictum venenatis. Vestibulum tempus tellus odio, id
-        elementum nibh egestas non. Integer feugiat ligula dolor. Donec nec eros
-        elementum, auctor ante ut, sodales nisl. Fusce sem est, dictum non
-        feugiat quis, volutpat at libero. Fusce molestie laoreet urna non
-        tincidunt. Curabitur pharetra est ornare pulvinar facilisis. Quisque
-        aliquam tristique euismod. Nullam et bibendum mi.
-      </p>
+      <h3 className="mt-5 fw-bold">{site.name}</h3>
+      <p className="lh-lg fs-6">{site.description}</p>
       <div className="p-3 my-sm-5 text-sm-center commentContainer">
         <h5>LOCATION</h5>
         <h5 className="mt-5 mb-3">COMMENTS</h5>
