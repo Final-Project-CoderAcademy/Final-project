e39e53fda66cf616a020494b1539783889963114
yuka toshima <tb19321yj@outlook.com>
2022-11-14 15:58:49 +1100

    users list for admin pnly page and delete common user function

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,7 +3,6 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
-  USER_REGISTER_RESET,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
@@ -83,7 +82,7 @@ export const logIn = (email, password) => async (dispatch) => {
 export const logOut = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
-  dispatch({ type: USER_REGISTER_RESET });
+  dispatch({ type: USER_LIST_RESET });
 };
 
 // Get user profile info
@@ -132,6 +131,57 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: USER_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// Get all the user info
+export const listUsers = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_LIST_REQUEST });
+    const {
+      userLogIn: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(`/api/users`, config);
+    dispatch({ type: USER_LIST_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// Delete one user
+export const deleteUser = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: USER_DELETE_REQUEST });
+    // get userInfo from the current user who already log in
+    const {
+      userLogIn: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.delete(`/api/users/${id}`, config);
+    dispatch({ type: USER_DELETE_SUCCESS });
+  } catch (error) {
+    dispatch({
+      type: USER_DELETE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/pages/AdminUserList.js
+++ frontend/src/pages/AdminUserList.js
@@ -1,31 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro"; // <-- import styles to be used
 import { Container, Button } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
+import { listUsers, deleteUser } from "../actions/userActions";
 
 const AdminUserList = () => {
-  const dammyUsers = [
-    {
-      id: "10000000000",
-      username: "ABC",
-      email: "user1@example.com",
-      admin: false,
-    },
-    {
-      id: "20000000000",
-      username: "DEF",
-      email: "user2@example.com",
-      admin: true,
-    },
-    {
-      id: "30000000000",
-      username: "GHI",
-      email: "user3@example.com",
-      admin: false,
-    },
-  ];
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+  const userLogIn = useSelector((state) => state.userLogIn);
+  const { userInfo } = userLogIn;
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listUsers());
+    } else {
+      navigate("/login");
+    }
+  }, [dispatch, navigate, userInfo, successDelete]);
+
+  //delete common user
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure?")) {
+      dispatch(deleteUser(id));
+    }
+  };
 
   return (
     <Container className="justify-content-md-center">
@@ -38,57 +44,64 @@ const AdminUserList = () => {
         />
       </Link>
       <h2 className="mb-4 text-center">USER LIST</h2>
-
-      <Table
-        striped
-        bordered
-        hover
-        responsive
-        className=" table-sm text-center my-5 text-nowrap"
-      >
-        <thead>
-          <tr>
-            <th className="p-3">ID</th>
-            <th className="p-3">USERNAME</th>
-            <th className="p-3">EMAIL</th>
-            <th className="p-3">ADMIN</th>
-            <th className="p-3"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {dammyUsers.map(({ id, username, email, admin }) => (
-            <tr key={id}>
-              <td className="p-3">{id}</td>
-              <td className="p-3">{username}</td>
-              <td className="p-3">{email}</td>
-              {admin ? (
-                <td className="p-3">
-                  <FontAwesomeIcon
-                    icon={solid("check")}
-                    style={{ color: "green" }}
-                  />
-                </td>
-              ) : (
-                <td className="p-3">
-                  <FontAwesomeIcon
-                    icon={solid("xmark")}
-                    style={{ color: "red" }}
-                  />
-                </td>
-              )}
-              {admin ? (
-                <td className="p-3">Admin</td>
-              ) : (
-                <td className="p-3">
-                  <Button variant="danger" className="btn-sm">
-                    <FontAwesomeIcon icon={solid("trash-can")} />
-                  </Button>
-                </td>
-              )}
+      {error ? (
+        <p>{error}</p>
+      ) : (
+        <Table
+          striped
+          bordered
+          hover
+          responsive
+          className=" table-sm text-center my-5 text-nowrap"
+        >
+          <thead>
+            <tr>
+              <th className="p-3">ID</th>
+              <th className="p-3">USERNAME</th>
+              <th className="p-3">EMAIL</th>
+              <th className="p-3">ADMIN</th>
+              <th className="p-3"></th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {users?.map((user) => (
+              <tr key={user._id}>
+                <td className="p-3">{user._id}</td>
+                <td className="p-3">{user.name}</td>
+                <td className="p-3">{user.email}</td>
+                {user._isAdmin ? (
+                  <td className="p-3">
+                    <FontAwesomeIcon
+                      icon={solid("check")}
+                      style={{ color: "green" }}
+                    />
+                  </td>
+                ) : (
+                  <td className="p-3">
+                    <FontAwesomeIcon
+                      icon={solid("xmark")}
+                      style={{ color: "red" }}
+                    />
+                  </td>
+                )}
+                {user.isAdmin ? (
+                  <td className="p-3">Admin</td>
+                ) : (
+                  <td className="p-3">
+                    <Button
+                      variant="danger"
+                      className="btn-sm"
+                      onClick={() => deleteHandler(user._id)}
+                    >
+                      <FontAwesomeIcon icon={solid("trash-can")} />
+                    </Button>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </Container>
   );
 };
