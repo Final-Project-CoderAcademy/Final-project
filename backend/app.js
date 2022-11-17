d2c00c471fc3771f04e98ef5a386ee76ef8d041d
Todd0554 <toddy9229@outlook.com>
2022-11-18 10:50:35 +1100

    reset Secure Node.js with a Content Security Policy

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(function (req, res, next) {
   res.setHeader(
     'Content-Security-Policy-Report-Only',
-    "default-src 'self'; font-src 'self'; img-src 'self' https://images.unsplash.com/photo-1515861461225-1488dfdaf0a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80; script-src 'self' https://kit.fontawesome.com/56a258cb08.js https://maps.googleapis.com/maps/api/js?callback=__googleMapsCallback&key=AIzaSyC_mJ_HcnWHVnTY4RmWl6U8t2C6r-jdJTY&libraries=places; style-src 'self'; frame-src 'self'"
+    "default-src 'self' https://ka-f.fontawesome.com; font-src 'self' https://ka-f.fontawesome.com; img-src 'self' https://images.unsplash.com https://maps.gstatic.com; script-src 'self' https://kit.fontawesome.com/56a258cb08.js https://maps.googleapis.com; style-src 'self' https://fonts.googleapis.com/; frame-src 'self'"
   );
   next();
 });
