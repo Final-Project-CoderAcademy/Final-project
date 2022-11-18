2e3bad9290b44d9efdb3123955bcfafdad367f60
Todd0554 <toddy9229@outlook.com>
2022-11-18 11:51:10 +1100

    reset Secure Node.js with a Content Security Policy && fix sign up with the email already exist bug

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -25,28 +25,38 @@ import {
 
 // register POST request actions
 export const register = (name, email, password) => async (dispatch) => {
-  // try {
-  dispatch({ type: USER_REGISTER_REQUEST });
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  const { data } = await axios.post(
-    "/api/users/register",
-    { name, email, password },
-    config
-  );
-  dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
-  if (!data.errors) {
-    dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
-    localStorage.setItem("userInfo", JSON.stringify(data));
-  } else {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/api/users/register",
+      { name, email, password },
+      config
+    );
+    dispatch({ type: USER_REGISTER_SUCCESS, payload: data });
+    if (!data.errors) {
+      dispatch({ type: USER_LOGIN_SUCCESS, payload: data });
+      localStorage.setItem("userInfo", JSON.stringify(data));
+    } else {
+      dispatch({
+        type: USER_REGISTER_FAIL,
+        payload: data.errors,
+      });
+    }
+  } catch (error) {
     dispatch({
       type: USER_REGISTER_FAIL,
-      payload: data.errors,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
     });
   }
+
 };
 
 // login POST request actions
