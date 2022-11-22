a109fbed2570d8600b53da81eaffe9a06d18a0bd
yuka toshima <tb19321yj@outlook.com>
2022-11-22 16:31:04 +1100

    some test

--- frontend/cypress/e2e/sitesList.cy.js
+++ frontend/cypress/e2e/sitesList.cy.js
@@ -10,4 +10,10 @@ describe("<SitesList />", () => {
     ).click();
     cy.url().should("contain", "/login");
   });
+  it("should visit site detail page by clicking 'Read More' button if the user is NOT logged in", () => {
+    cy.get(
+      ":nth-child(1) > .m-3 > .card-body > a > .text-end > .btn-round"
+    ).click();
+    cy.url().should("contain", "/login");
+  });
 });
